08a4e6f13bc529d43080b4449da839c3df6ce255
moses <moses.kingi@moringaschool.com>
2023-11-02 05:41:56 +0300

    adding the display of the typed message for decision making purpose

--- docs/index.js
+++ docs/index.js
@@ -1,43 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
     const baseUrl = "http://localhost:3000";
     console.log("API base URL:", baseUrl);
-///The bASEuRL,the DomContentLoaded Eventlistener to show that the Dom is Loaded//
-        ////Navbar EVENT LISTENERS making them  not need to persist after reloading the page.//
 
-        const homeLink = document.getElementById('homeLink');
-        const galleryLink = document.getElementById('galleryLink');
-        const servicesLink = document.getElementById('servicesLink');
-        const aboutUsLink = document.getElementById('aboutUsLink');
-        const contactUsLink = document.getElementById('contactUsLink');
-        const contentDiv = document.getElementById('content');
-    
-        homeLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            contentDiv.innerHTML = `BUTTON COMING SOON!!!!.`;
-        });
-    
-        galleryLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            contentDiv.innerHTML = `BUTTON COMING SOON!!.`;
-        });
-    
-        servicesLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            contentDiv.innerHTML = `Services button under maintenance!!.`;
-        });
-    
-        aboutUsLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            contentDiv.innerHTML = `About Us button under maintenance!!.`;
-        });
-    
-        contactUsLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            contentDiv.innerHTML = `Contact Us button under maintenance!!`;
-        });
-    
-    })
-///to make the selection empty on seleting a court name on the courts///
+    const homeLink = document.getElementById('homeLink');
+    const galleryLink = document.getElementById('galleryLink');
+    const servicesLink = document.getElementById('servicesLink');
+    const aboutUsLink = document.getElementById('aboutUsLink');
+    const contactUsLink = document.getElementById('contactUsLink');
+    const contentDiv = document.getElementById('content');
+
+    homeLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        contentDiv.innerHTML = `BUTTON COMING SOON!!!!.`;
+    });
+
+    galleryLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        contentDiv.innerHTML = `BUTTON COMING SOON!!.`;
+    });
+
+    servicesLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        contentDiv.innerHTML = `Services button under maintenance!!.`;
+    });
+
+    aboutUsLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        contentDiv.innerHTML = `About Us button under maintenance!!.`;
+    });
+
+    contactUsLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        contentDiv.innerHTML = `Contact Us button under maintenance!!`;
+    });
+
     function hideOptions() {
         const selectElement = document.getElementById('court');
         const selectedValue = selectElement.value;
@@ -47,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
-  // Code that executes(Variables with stored reference in the element by IDS) on form submission that is on clicking send(eventListeners) the message to show too form the input fields//
+
     const form = document.getElementById('contact-form');
     const sendButton = document.getElementById('sendButton');
 
@@ -69,9 +65,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function showPopup() {
-        alert('Your message has been  we value your feedback!');
+        alert('Your message has been sent. We value your feedback!');
     }
-///Communicating with the server using Public Api key// 
+
     function postToApi(data) {
         const url = 'https://sentiment-analysis40.p.rapidapi.com/api/sentiment';
         const options = {
@@ -86,48 +82,77 @@ document.addEventListener('DOMContentLoaded', function () {
                 text: document.getElementById('message').value
             })
         };
-    
+
         fetch(url, options)
             .then(response => response.json())
             .then(result => {
                 console.log("Sentiment Analysis Result:", result);
-
-                ///Displaying the posted information/the fetched Public Api data by accessing the  the sentiment data using result.result.label and result.result.score/delete buttons
-                const structuredData = {
-                    ...data,
-                    sentiment: result.result.label,
-                    score: result.result.score
-                };
-                console.log("Structured Data:", structuredData);
-                const resultList = document.getElementById('result-list');
-                resultList.innerHTML = '';
-
-                for (const [key, value] of Object.entries(structuredData)) {
-                    const listItem = document.createElement('li');
-                    const deleteButton = document.createElement('button'); // Create the delete button
-                    deleteButton.textContent = 'Delete';
-                    deleteButton.style.backgroundColor = '#f44336'; // styling the dynamically added delete button Red
-                    deleteButton.addEventListener('click', () => {
-                      listItem.remove(); // Remove the list item when the delete button is clicked
-                    });
-                    listItem.textContent = `${key}: ${value}`;
-                    listItem.appendChild(deleteButton); // Appending the delete button to the list item
-                    resultList.appendChild(listItem);
-                  }
+                handleSentiments(data, result);
             })
             .catch(error => {
                 console.error(error);
             });
     }
-    // the social Media links variables stored inn references by the Elements by IDS, making them  not need to persist after reloading the page.//
+
+    function handleSentiments(data, result) {
+        const structuredData = {
+            ...data,
+            sentiment: result.result.label,
+            score: result.result.score
+        };
+        console.log("Structured Data:", structuredData);
+        const resultList = document.getElementById('result-list');
+        resultList.innerHTML = '';
+
+        const positiveList = document.getElementById('positiveList');
+        const negativeList = document.getElementById('negativeList');
+
+        for (const [key, value] of Object.entries(structuredData)) {
+            const listItem = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.style.backgroundColor = '#f44336';
+            deleteButton.addEventListener('click', () => {
+                listItem.remove();
+            });
+            listItem.textContent = `${key}: ${value}`;
+            listItem.appendChild(deleteButton);
+            resultList.appendChild(listItem);
+
+            if (value === 'positive') {
+                const positiveItem = document.createElement('li');
+                positiveItem.textContent = key;
+                positiveList.appendChild(positiveItem);
+                makePositiveDecision();
+            } else if (value === 'negative') {
+                const negativeItem = document.createElement('li');
+                negativeItem.textContent = key;
+                negativeList.appendChild(negativeItem);
+                makeNegativeDecision();
+            }
+        }
+    }
 
     const youtubeLink = document.getElementById('youtubeLink');
     const facebookLink = document.getElementById('facebookLink');
     const twitterLink = document.getElementById('twitterLink');
     const helpLink = document.getElementById('helpLink');
-    const socialMediaContentDiv = document.getElementById('socialMediaContent');
 
-    // The event listeners for youtubeLink, facebookLink, twitterLink, and helpLink
+    youtubeLink.addEventListener('click', function () {
+        loadSocialMedia('YouTube');
+    });
+
+    facebookLink.addEventListener('click', function () {
+        loadSocialMedia('Facebook');
+    });
+
+    twitterLink.addEventListener('click', function () {
+        loadSocialMedia('Twitter');
+    });
+
+    helpLink.addEventListener('click', function () {
+        loadSocialMedia('Help');
+    });
 
     function loadContent(page) {
         const contentDiv = document.getElementById('content');
@@ -138,4 +163,36 @@ document.addEventListener('DOMContentLoaded', function () {
         const socialMediaContentDiv = document.getElementById('socialMediaContent');
         socialMediaContentDiv.innerHTML = `You clicked on the ${platform} link.`;
     }
+
+    function makePositiveDecision() {
+        console.log('The sentiment analysis indicates a positive response.');
+        displayPositiveMessage();
+    }
+
+    function makeNegativeDecision() {
+        console.log('The sentiment analysis indicates a negative response.');
+        displayNegativeWarning();
+    }
+
+    function displayPositiveMessage() {
+        const positiveMessage = document.createElement('p');
+        positiveMessage.textContent = 'Thank you for your positive feedback!';
+        positiveMessage.style.color = 'green';
+        const messageContainer = document.getElementById('messageContainer');
+        messageContainer.appendChild(positiveMessage);
+    }
+
+    function displayNegativeWarning() {
+        const warningMessage = document.createElement('p');
+        warningMessage.textContent = 'We apologize for any inconvenience. Please contact us for further assistance.';
+        warningMessage.style.color = 'red';
+        const messageContainer = document.getElementById('messageContainer');
+        messageContainer.appendChild(warningMessage);
+    }
+});
+   
+
+
+
+
 
